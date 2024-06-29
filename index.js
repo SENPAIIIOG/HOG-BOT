d1b5222e891f96d74481e8abc0640d534f27dc75
SENPAIII <149959627+SENPAIIIOG@users.noreply.github.com>
2024-06-29 14:35:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["WELCOME GUYS🤘","VERIFY✅","READ RULES🛑","FOLLOW"];
 
 
 let currentIndex = 0;
